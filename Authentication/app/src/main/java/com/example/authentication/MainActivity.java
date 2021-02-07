503c6929ea86ad78bbd7079c8a63d704c9b74595
Lucas Brière <lucas.briere@efrei.net>
2021-02-07 12:46:10 +0100

    Ex1 and Ex 2

---
+++ Authentication/app/src/main/java/com/example/authentication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onAuthenticate(View v) {
+    }
+}
