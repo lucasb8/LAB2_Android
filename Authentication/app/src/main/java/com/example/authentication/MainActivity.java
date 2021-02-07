86bb1620090a5f2b4a82d1e4fe217af8ffcfe771
Lucas Brière <lucas.briere@efrei.net>
2021-02-07 13:17:04 +0100

    Ex 3

--- Authentication/app/src/main/java/com/example/authentication/MainActivity.java
+++ Authentication/app/src/main/java/com/example/authentication/MainActivity.java
@@ -3,21 +3,15 @@ package com.example.authentication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +22,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onAuthenticate(View v) {
+        URL url;
+        try {
+            // Creation of the url to access to the website
+            url = new URL("http://www.android.com/");
+            // Open the connection
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                // Implementation of the buffer to read element
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                // Stock the data in a string
+                String s = readStream(in);
+                // Print the data
+                Log.i("JFL", s);
+            } finally {
+                // Close the connection at the end of the usage
+                urlConnection.disconnect();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Provide access to the typewritten data on an input stream
+    private String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader r = new BufferedReader(new InputStreamReader(is),1000);
+        // Browser of the input bufferedReader
+        for (String line = r.readLine(); line != null; line =r.readLine()){
+            sb.append(line);
+        }
+        is.close();
+        // Return the data in string type
+        return sb.toString();
     }
 }
