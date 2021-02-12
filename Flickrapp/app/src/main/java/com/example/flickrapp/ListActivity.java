7941822a746978c8326d094e2ac97b557c61edad
Lucas Brière <lucas.briere@efrei.net>
2021-02-12 11:41:37 +0100

    Ex 20

--- Flickrapp/app/src/main/java/com/example/flickrapp/ListActivity.java
+++ Flickrapp/app/src/main/java/com/example/flickrapp/ListActivity.java
@@ -3,12 +3,21 @@ package com.example.flickrapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        adapter = new MyAdapter();
+
+        // Retrieve the list view and link withe the adapter
+        ListView list = findViewById(R.id.list);
+        list.setAdapter(adapter);
     }
 }
