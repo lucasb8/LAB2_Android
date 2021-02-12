3765ae0bb63cfb4a0d44131c195bba41aae5cc1a
Lucas Brière <lucas.briere@efrei.net>
2021-02-12 18:09:45 +0100

    All exercises until 31

--- Flickrapp/app/src/main/java/com/example/flickrapp/MyAdapter.java
+++ Flickrapp/app/src/main/java/com/example/flickrapp/MyAdapter.java
@@ -1,15 +1,17 @@
 package com.example.flickrapp;
 
+import android.graphics.Bitmap;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.ImageRequest;
 
-import java.security.AccessController;
 import java.util.Vector;
 
 public class MyAdapter extends BaseAdapter {
@@ -23,7 +25,7 @@ public class MyAdapter extends BaseAdapter {
     }
 
 
-    public void add(String url){
+    void add(String url){
         // Store the URL
         vector.add(url);
 
@@ -49,16 +51,44 @@ public class MyAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        // Get a RequestQueue
-        RequestQueue queue = MySingleton.getInstance(parent.getContext()).getRequestQueue();
-
         // Inflate the layout
+        /*
         convertView = LayoutInflater.from(parent.getContext())
                     .inflate(R.layout.textviewlayout, parent, false);
+
         // Retrieve the urlText
         TextView text = convertView.findViewById(R.id.urlText);
         // Add url into the TextView
+        String stringURL = vector.get(position);
         text.setText(vector.get(position));
+
+         */
+
+
+        // Get a RequestQueue
+        RequestQueue queue = MySingleton.getInstance(parent.getContext()).getRequestQueue();
+
+        convertView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.bitmaplayout, parent, false);
+
+        // Retrieve the url of the image
+        String stringURL = vector.get(position);
+        // retrieve the image view container
+        ImageView image = convertView.findViewById(R.id.imageUrl);
+
+
+        // create a response listener
+        Response.Listener<Bitmap> responseListener = bmp -> {
+            image.setImageBitmap(bmp);
+        };
+
+        // Create the image request
+        ImageRequest request = new ImageRequest(stringURL, responseListener,
+                300, 300, ImageView.ScaleType.CENTER,
+                Bitmap.Config.RGB_565, null);
+
+        // Add the request to the queue
+        queue.add(request);
 
         return convertView;
     }
